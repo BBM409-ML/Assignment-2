e83517dad0e5a798adf128ed49ea879ca6cbc2e0
Betul Karagoz <44498862+betulkaraagoz@users.noreply.github.com>
2021-11-14 21:12:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import pprint
-import random
-
-import pandas
 from math import log
+
 import numpy as np
+import pandas
 import pandas as pd
 from scipy.linalg.decomp_schur import eps
 from sklearn.model_selection import KFold, train_test_split
@@ -57,7 +55,7 @@ def find_max_info_gain(df):
 		# Gain(S,A) = Entropy(S) - sum(|S column|/|S|) Entropy(S column)
 		information_gains.append(find_total_entropy(df) - find_entropy_attribute(df, key))
 	# return max info gain = most effective attribute in classifying training data
-	return df.keys()[:-1][np.argmax(information_gains)]
+	return df.keys()[:-1][np.argmax(information_gains)], max(information_gains)
 
 
 # this function gets sub dataframe including columns where node = given value
@@ -66,17 +64,20 @@ def get_sub_dataframe(df, node, value):
 
 
 def build_tree(df, tree=None):
+	global count
 	# take target value = Class[Positive, Negative]
 	Class = df.keys()[-1]
 	# in order to decide next node, find attribute with max information gain
-	node = find_max_info_gain(df)
+	node, info_gain = find_max_info_gain(df)
 	# get distinct values for that attribute like Yes, No, Positive, Negative
 	distinct_values = df[node].unique()
 
 	# create empty dictionary for tree representation
 	if tree is None:
+		count += 1
 		tree = {}
 		tree[node] = {}
+		tree[node][count] = info_gain
 
 	for value in distinct_values:
 		sub_data_frame = get_sub_dataframe(df, node, value)
@@ -137,19 +138,14 @@ def k_fold_cross_validation(data):
 		data_train, data_test = data.iloc[train_index].reset_index(drop=True), \
 								data.iloc[test_index].reset_index(drop=True)
 		print("############################################################")
+		print("PART 1")
 		print()
 		count = 0
 		data_train_2 = data_train
 		tree = build_tree(data_train)
-		print(tree)
-		numerated_tree = enumerate_nodes(tree, {})
-		pprint.pprint(numerated_tree)
-		print(prune(tree, {}, numerated_tree))
-		visualize(tree)
+		# visualize(tree)
 		accuracy = evaluate(tree, data_test, "Class")
 		print(accuracy)
-		print()
-		print("############################################################")
 
 
 def train_validation_test_splitter(dataframe):
@@ -172,32 +168,89 @@ def is_twig(node):
 	return True
 
 
-def prune(tree, twigs, numerated_tree):
+def find_twigs(tree, twigs):
 	root_node = next(iter(tree))
 
 	if is_twig(tree[root_node]):
-		twigs[random.randint(1, 500)] = tree[root_node]  # add info gain
+		twigs.append(tree[root_node])  # add info gain
 	else:
 		for value in tree[root_node].keys():  # checking the feature value in current tree node
 			if is_leaf(tree[root_node][value]):
 				continue
-			prune(tree[root_node][value], twigs, numerated_tree)  # goto next feature
+			find_twigs(tree[root_node][value], twigs)  # goto next feature
 
 	return twigs
 
 
-def enumerate_nodes(tree, enumerated):
-	global count
+def find_least_info_gain_twig(twigs):
+	min_info_gain = 1
+	node_num = 0
+
+	for twig in twigs:
+		counter = 0
+		for key, value in twig.items():
+			if counter == 0:
+				if value < min_info_gain:
+					min_info_gain = value
+					node_num = key
+			counter += 1
+
+	return node_num
+
+
+def delete_children(tree, node_num):
 	root_node = next(iter(tree))
 
 	for value in tree[root_node].keys():
-		enumerated[count] = tree[root_node][value]
-		count += 1
 		if is_leaf(tree[root_node][value]):
 			continue
-		enumerate_nodes(tree[root_node][value], enumerated)
+		if node_num == get_node_num(tree[root_node][value]):
+			tree[root_node][value] = get_node_value(tree[root_node][value])
+			return tree
+		else:
+			delete_children(tree[root_node][value], node_num)
 
-	return enumerated
+	return tree
+
+
+def get_node_num(node):
+	root_node = next(iter(node))
+
+	counter = 0
+	for key, value in node[root_node].items():
+		if counter == 0:
+			return key
+
+
+## BURADA DAL BİR DEĞİL BİRDEN FAZLA OLABİLİR Yes: Positive, No: Positive gibi
+## BU DURUMDA majortiy seçilmeli
+def get_node_value(node):
+	root_node = next(iter(node))
+
+	counter = 0
+	for key, value in node[root_node].items():
+		if counter == 1:
+			return value
+		counter += 1
+
+
+def prune(data):
+	global data_train_2
+	print("###################################################")
+	train, test, validation = train_validation_test_splitter(data)
+	data_train_2 = train
+	tree = build_tree(train)
+	last_accuracy = evaluate(tree, test.reset_index(drop=True), "Class")
+	print(last_accuracy)
+	twigs = find_twigs(tree, [])
+	node_num = find_least_info_gain_twig(twigs)
+	tree = delete_children(tree, node_num)
+	current_accuracy = evaluate(tree, validation.reset_index(drop=True), "Class")
+	print(current_accuracy)
+	print()
+	print("-------------------------------------------------------")
+	print()
+	visualize(tree)
 
 
 def main():
@@ -208,8 +261,11 @@ def main():
 	data['Age'] = data['age_discrete']
 	# drop unnecessary column
 	data = data.drop(columns="age_discrete")
-	k_fold_cross_validation(data)
-	# train_validation_test_splitter(data)
+	#k_fold_cross_validation(data)
+	prune(data)
+
+
+# train_validation_test_splitter(data)
 
 
 count = 0
